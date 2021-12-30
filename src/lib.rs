19f71a46b4fb69e4d0ccffb2cdc84091c28d3707
Gil Engel <gil.engel@web.de>
2021-12-30 14:39:57 +0100

    Add street intersection constrained

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,64 @@
-use std::cell::Cell;
-use std::cell::RefCell;
-use std::cell::RefMut;
-use std::rc::Rc;
+use std::cmp::Ordering;
+
+use geo::line_intersection::LineIntersection;
+use geo::line_intersection::line_intersection;
+use geo::prelude::EuclideanDistance;
+use geo_types::Coordinate;
+use geo_types::Line;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use geo::euclidean_distance;
 
 use geo_types::Point;
+
 use web_sys::CanvasRenderingContext2d;
 use web_sys::HtmlCanvasElement;
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 struct Street {
-    start: Point<f64>,
-    end: Point<f64>,
+    line: Line<f64>,
+
+    width: f64,
 }
 
+
 impl Street {
+    pub fn start(&self) -> Coordinate<f64> {
+        self.line.start
+    }
+
+    pub fn set_start(&mut self, start: Coordinate<f64>) {
+        self.line.start = start;
+    }
+
+    pub fn end(&self) -> Coordinate<f64> {
+        self.line.end
+    }
+
+    pub fn set_end(&mut self, end: Coordinate<f64>) {
+        self.line.end = end
+    }
+
     pub fn render(&self, context: &CanvasRenderingContext2d) {
         context.begin_path();
-        context
-            .move_to(self.start.x(), self.start.y());
-        context
-            .line_to(self.end.x(), self.end.y());
+        context.move_to(self.start().x, self.start().y);
+        context.line_to(self.end().x, self.end().y);
+        context.set_line_width(self.width);
         context.stroke();
+    }
+
+    pub fn intersect_with_street(&self, another: &Street) -> Option<LineIntersection<f64>> {
+        line_intersection(self.line, another.line)
     }
 }
 
 impl Default for Street {
-    fn default() -> Self { Street { start: Point::new(0.0, 0.0), end: Point::new(0.0, 0.0) } }
+    fn default() -> Self {
+        Street {
+            line: Line::new(Point::new(0.0, 0.0), Point::new(0.0, 0.0)),
+            width: 20.0
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -38,7 +69,6 @@ pub struct Editor {
 
     temp_street: Street,
 
-    canvas: HtmlCanvasElement,
     context: CanvasRenderingContext2d,
 
     mouse_pressed: bool,
@@ -63,18 +93,14 @@ fn get_canvas_and_context() -> Result<(HtmlCanvasElement, CanvasRenderingContext
 #[wasm_bindgen]
 impl Editor {
     pub fn new() -> Editor {
-        let (canvas, context) = get_canvas_and_context().unwrap();
+        let (_, context) = get_canvas_and_context().unwrap();
         Editor {
             width: 1920,
             height: 800,
             streets: vec![],
-            temp_street: Street {
-                start: Point::new(0.0, 0.0),
-                end: Point::new(0.0, 0.0),
-            },
-            canvas,
+            temp_street: Street::default(),
             context,
-            mouse_pressed: false
+            mouse_pressed: false,
         }
     }
 
@@ -87,7 +113,8 @@ impl Editor {
     }
 
     pub fn render(&self) {
-        self.context.clear_rect(0.0, 0.0, 1920.0, 1080.0);
+        self.context
+            .clear_rect(0.0, 0.0, self.width.into(), self.height.into());
 
         self.temp_street.render(&self.context);
 
@@ -97,19 +124,15 @@ impl Editor {
     }
 
     pub fn mouse_down(&mut self, x: u32, y: u32) {
-        self.temp_street.start.set_x(x as f64);
-        self.temp_street.start.set_y(y as f64);
-
-        self.temp_street.end.set_x(x as f64);
-        self.temp_street.end.set_y(y as f64);
+        self.temp_street.set_start(Coordinate { x: x.into(), y: y.into() });
+        self.temp_street.set_end(Coordinate { x: x.into(), y: y.into() });
 
         self.mouse_pressed = true
     }
 
     pub fn mouse_up(&mut self, x: u32, y: u32) {
         if self.mouse_pressed {
-            self.temp_street.end.set_x(x as f64);
-            self.temp_street.end.set_y(y as f64);
+            //self.temp_street.set_end(Coordinate { x: x.into(), y: y.into() });
 
             self.streets.push(self.temp_street.clone());
 
@@ -121,74 +144,50 @@ impl Editor {
 
     pub fn mouse_move(&mut self, x: u32, y: u32) {
         if self.mouse_pressed {
-            self.temp_street.end.set_x(x as f64);
-            self.temp_street.end.set_y(y as f64);
+            self.temp_street.set_end(Coordinate { x: x.into(), y: y.into() });
+
+            if let Some(intersection) = self.intersect_with_streets() {
+                self.temp_street.set_end(intersection)
+            }
         }
     }
 
-    /*
+    fn intersect_with_streets(&self) -> Option<Coordinate<f64>> {
+        let mut intersections = vec![];
 
-    pub fn register_mouse_events(&self) -> Result<(), JsValue>  {
-        let (canvas, context) = self.get_canvas_and_context().unwrap();
-
-        //let mut temp_street = Rc::new(Cell::new(Street { start: Point::new(0.0, 0.0), end: Point::new(0.0, 0.0) }));
-        let start = Rc::new(Cell::new(Point::new(0.0, 0.0)));
-        let end = Rc::new(Cell::new(Point::new(0.0, 0.0)));
-        let pressed = Rc::new(Cell::new(false));
-        {
-            let pressed = pressed.clone();
-            let start = start.clone();
-            let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-
-                start.set(Point::new(event.offset_x() as f64, event.offset_y() as f64));
-
-                pressed.set(true);
-            }) as Box<dyn FnMut(_)>);
-            canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
-            closure.forget();
-        }
-        {
-            let context = context.clone();
-            let start = start.clone();
-            let end = end.clone();
-            let pressed = pressed.clone();
-            let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-                if pressed.get() {
-                    end.set(Point::new(event.offset_x() as f64, event.offset_y() as f64));
-                    context.clear_rect(0.0, 0.0, 1920.0, 1080.0);
-                    context.begin_path();
-                    context.move_to(start.get().x(), start.get().y());
-                    context.line_to(end.get().x(), end.get().y());
-                    context.stroke();
-
+        for street in &self.streets {            
+            if let Some(line_intersection) = self.temp_street.intersect_with_street(street) {
+                match line_intersection {
+                    LineIntersection::SinglePoint { intersection, is_proper } => 
+                    {
+                        if is_proper {
+                            intersections.push(intersection);
+                        }
+                    },
+                    _ => {},
                 }
-            }) as Box<dyn FnMut(_)>);
-            canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
-            closure.forget();
-        }
-        {
-            let context = context.clone();
-            let pressed = pressed.clone();
-            let start = start.clone();
-            let end = end.clone();
-            //let streets = Rc<RefCell<_>> = Rc::new(RefCell::new(self.streets));
-            let streets: Rc<RefCell<_>> = Rc::new(RefCell::new(self.streets.clone()));
-
-            //let mut streets = Cell::new(self.streets);
-            let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-                pressed.set(false);
-                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-                context.stroke();
-
-                let mut map: RefMut<_> = streets.borrow_mut();
-                map.push(Street { start: start.get(), end: end.get() });
-                //web_sys::console::log_1(&streets.len().to_string().into());
-            }) as Box<dyn FnMut(_)>);
-            canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
-            closure.forget();
+            }
         }
 
-        Ok(())
+        intersections.sort_by(|a, b| {
+            let d1 = a.euclidean_distance(&self.temp_street.start());
+            let d2 = b.euclidean_distance(&self.temp_street.start());
+
+            if d1 < d2 {
+                return Ordering::Less
+            }
+            
+            if d1 == d2 {
+                return Ordering::Equal
+            }
+
+            Ordering::Greater            
+        });
+
+        if intersections.is_empty() {
+            return None
+        }
+        
+        Some(intersections.first().unwrap().clone())
     }
-    */
 }
